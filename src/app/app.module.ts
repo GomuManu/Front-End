c678a05d6d5f4699c4fdea4fa9b703b11eee92b8
Manuel de Jesús Castro Vázquez <kirby_meta@hotmail.com>
2020-03-19 20:01:09 -0600

    E1 A1 A2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaCarrosComponent } from './lista-carros/lista-carros.component';
+import { HeaderComponent } from './header/header.component';
+import { VistasComponent } from './vistas/vistas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaCarrosComponent
+    ListaCarrosComponent,
+    HeaderComponent,
+    VistasComponent
   ],
   imports: [
     BrowserModule,
